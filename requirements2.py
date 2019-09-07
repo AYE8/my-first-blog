3189e69823fba7e606f5062b3fcf4ef9779d8daf
Your Namenauczycielbio <asiaa9@gmail.com>
2019-09-07 15:21:27 +0200

    My Django Girls app, first commit

---
+++ requirements2.py
@@ -0,0 +1,27 @@
+
+print("Hello, Django girls!")
+a=2
+b=3
+a<b
+if 3>2:
+	print("To dziala!")
+if 5>2:
+	print("5jest jednak wieksze od 2")
+else:("5 nie jest wieksze od 2")
+name="Sonja"
+if name =="Ola":
+	print("Hej Ola!")
+elif name =="Sonja":
+	print("Hej Sonja!")
+else: 
+	print("Hej anonimie!")
+volume=57
+if volume<20:
+	print("it's kinda quiet")
+elif 40 <=volume<60:
+	print("perfect")
+	def hi():
+    print('Hej!')
+    print('Jak się masz?')
+
+hi()
